3af4e59bd0692be0980f14d2adfc20f27e4e6655
Aradhya Nepal <aradhya.1221@gmail.com>
2023-08-16 11:20:10 +0545

    Windows and Android icon and name setup

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MultiPlayer TicTacToe by A2',
       restorationScopeId: "Test", // <-- Add this line
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"multiplayer_tictactoe", origin, size)) {
+  if (!window.Create(L"MultiPlayerTicTacToeByA2", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
